630cd5bfdf06fa94d7bee15335ed17c18290d71d
Servando Coria <servando.coria@freewayinsurance.com>
2016-02-24 13:18:12 -0800

    building the web api controller

---
+++ APM.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using APM.WebAPI.Models;
+
+namespace APM.WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET api/products
+        public IEnumerable<Product> Get()
+        {
+            var productRepository = new ProductRepository();
+
+            return productRepository.Retrieve();
+        }
+
+        // GET api/products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
