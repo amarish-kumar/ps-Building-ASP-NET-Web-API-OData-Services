030f0bf66671ac501d6182c11c289b6009e6e2cd
Servando Coria <servando.coria@freewayinsurance.com>
2016-02-25 13:10:50 -0800

    configuring the json formatter in a web api service

--- APM.WebAPI/App_Start/WebApiConfig.cs
+++ APM.WebAPI/App_Start/WebApiConfig.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using Newtonsoft.Json.Serialization;
+
 namespace APM.WebAPI
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            // serialize and transform property names to proper json names
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
