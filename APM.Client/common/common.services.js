d095601ffba627edf8de09bc455a46c67e8ede39
Servando Coria <servando.coria@freewayinsurance.com>
2016-02-25 08:01:47 -0800

    calling the web api from angular

---
+++ APM.Client/common/common.services.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    "use strict";
+    // dependency on $resource
+    angular
+        .module("common.services", ["ngResource"])
+        .constant("appSettings", {
+            serverPath: "http://localhost:56138"
+        });
+
+} ());
---
+++ APM.Client/common/productResource.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("common.services")
+        .factory("productResource", ["$resource", "appSettings", productResource]);// factory service
+
+
+    function productResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/products/:id");
+    };
+} ());
