a294c6cd4cb0a254aa4348587e5c39bb92d2dab1
Servando Coria <servando.coria@freewayinsurance.com>
2016-02-26 15:18:21 -0800

    using odata queries from angular

--- APM.Client/app/products/productListCtrl.js
+++ APM.Client/app/products/productListCtrl.js
@@ -9,8 +9,13 @@
         var vm = this;
 
         vm.searchCriteria = "GDN";
+        vm.sortProperty = "Price";
+        vm.sortDirection = "desc";
 
-        productResource.query({search: vm.searchCriteria}, function (data) {
+        //productResource.query({$skip:1, $top:3}, function (data) {
+        //productResource.query({ $filter: "contains(ProductCode, 'GDN')"}, function (data) {
+        //productResource.query({ $filter: "contains(ProductCode, 'GDN') and Price ge 5 and Price le 20" }, function (data) {
+        productResource.query({ $filter: "contains(ProductCode, 'GDN') and Price ge 5 and Price le 20", $orderby: "Price desc" }, function (data) {
             vm.products = data;
         });
     }
