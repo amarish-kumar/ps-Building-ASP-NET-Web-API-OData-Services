766b9312dccaf5d446932817f93af493b71d9dc9
Servando Coria <servando.coria@freewayinsurance.com>
2016-03-03 07:56:09 -0800

    building a login form

---
+++ APM.Client/app/mainCtrl.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("productManagement")
+        .controller("MainCtrl", ["userAccount", MainCtrl]); // dependencia con userAccount
+
+    function MainCtrl(userAccount) {
+        var vm = this;
+        vm.isLoggedIn = false;
+        vm.message = '';
+        vm.userData = {
+            userData: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        };
+        vm.registerUser = function () {
+        }
+        vm.login = function () { 
+        }
+    }
+})();
