24f8d96ca578c6e04a9645687a1bf6d3543e4788
Servando Coria <servando.coria@freewayinsurance.com>
2016-03-04 15:51:41 -0800

    registering the user

--- APM.Client/app/mainCtrl.js
+++ APM.Client/app/mainCtrl.js
@@ -16,8 +16,30 @@
             confirmPassword: ''
         };
         vm.registerUser = function () {
+            vm.userData.confirmPassword = vm.userData.password;
+
+            userAccount.registerUser(vm.userData,
+                function (data) {
+                    vm.confirmPassword = "";
+                    vm.message = "... Registration successful";
+                    vm.login();
+                },
+                function (response) {
+                    vm.isLoggedIn = false;
+                    vm.message = response.statusText + "\r\n";
+                    if (response.data.exceptionMessage)
+                        vm.message += response.data.exceptionMessage;
+                    // validation errors
+                    if (response.data.modelState) {
+                        for (var key in response.data.modelState) {
+                            vm.message += response.data.modelState[key] + "\r\n";
+                        }
+                    }
+                    
+                }
+            );
         }
-        vm.login = function () { 
+        vm.login = function () {
         }
     }
 })();
